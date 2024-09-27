aba2ca9896bc197873772b856dd291528daf3b27
cer-rules <rules.cer.yamatomato@gmail.com>
2024-09-27 11:29:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // デバイス言語が日本語の場合は `/ja` にリダイレクト
   if (userLang.startsWith('ja')) {
-    window.location.href = 'https://rules.cer.f5.si/advertising/ja/index.html';
+    window.location.href = 'https://rules.cer.f5.si/advertising/ja';
   } else {
     // それ以外の場合は `/en` にリダイレクト
-    window.location.href = 'https://rules.cer.f5.si/advertising/en/index.html';
+    window.location.href = 'https://rules.cer.f5.si/advertising/en';
   }
 });
