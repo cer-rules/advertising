cb3b5f6722eaa7cac8bde9e4dc8b0842b410e0f1
cer-rules <rules.cer.yamatomato@gmail.com>
2024-09-27 10:32:29 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', () => {
+  // デバイスのデフォルト言語を取得
+  const userLang = navigator.language || navigator.userLanguage;
+
+  // デバイス言語が日本語の場合は `/ja` にリダイレクト
+  if (userLang.startsWith('ja')) {
+    window.location.href = '/ja';
+  } else {
+    // それ以外の場合は `/en` にリダイレクト
+    window.location.href = '/en';
+  }
+});
